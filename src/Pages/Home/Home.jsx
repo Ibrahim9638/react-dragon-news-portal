7b4a3c0f64362b2a5f40aae5cb93e317c39cf00d
Ibrahim9638 <hossain.ibrahim0123@gmail.com>
2024-09-01 12:15:47 +0600

    complete project

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,39 +1,38 @@
-import React, { useEffect, useState } from 'react';
-import Header from '../shared/Header/Header';
-import Navbar from '../shared/Navbar/Navbar';
-import LeftSideNav from '../shared/LeftSideNav/LeftSideNav';
-import RightSideNav from '../shared/RightSideNav/RightSideNav';
-import BreakingNews from './BreakingNews';
-import { useLoaderData } from 'react-router-dom';
-import NewsCard from '../NewsCard/NewsCard';
+import React, { useEffect, useState } from "react";
+import Header from "../shared/Header/Header";
+import Navbar from "../shared/Navbar/Navbar";
+import LeftSideNav from "../shared/LeftSideNav/LeftSideNav";
+import RightSideNav from "../shared/RightSideNav/RightSideNav";
+import BreakingNews from "./BreakingNews";
+import { useLoaderData } from "react-router-dom";
+import NewsCard from "../NewsCard/NewsCard";
 
 const Home = () => {
-    const news = useLoaderData();
+  const news = useLoaderData();
 
-    return (
+  return (
+    <div>
+      <Header />
+      <BreakingNews />
+      <Navbar />
+      <div className="grid grid-cols-1 md:grid-cols-4">
         <div>
-            <Header />
-            <BreakingNews />
-            <Navbar />
-            <div className='grid grid-cols-1 md:grid-cols-4'>
-                <div>
-                    <LeftSideNav />
-                </div>
-                <div className='md:col-span-2'>
-                    <h2 className='font-semibold text-xl mx-2 mb-4'>Dragon News</h2>
-                    <div>
-                        {
-                            news.map(singleNews=> <NewsCard 
-                                key={singleNews._id} singleNews={singleNews}></NewsCard>)
-                        }
-                    </div>
-                </div>
-                <div>
-                    <RightSideNav />
-                </div>
-            </div>
+          <LeftSideNav />
         </div>
-    );
+        <div className="md:col-span-2">
+          <h2 className="font-semibold text-xl mx-2 mb-4">Dragon News</h2>
+          <div>
+            {news.map((singleNews) => (
+              <NewsCard key={singleNews._id} singleNews={singleNews}></NewsCard>
+            ))}
+          </div>
+        </div>
+        <div>
+          <RightSideNav />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
--- src/Pages/News/News.jsx
+++ src/Pages/News/News.jsx
@@ -1,26 +1,55 @@
-import React from 'react';
-import Header from '../shared/Header/Header';
-import RightSideNav from '../shared/RightSideNav/RightSideNav';
-import { FaArrowLeft } from 'react-icons/fa';
-import { Link } from 'react-router-dom';
+import React, { useContext, useEffect, useState } from "react";
+import Header from "../shared/Header/Header";
+import RightSideNav from "../shared/RightSideNav/RightSideNav";
+import { FaArrowLeft } from "react-icons/fa";
+import { Link, useParams } from "react-router-dom";
 
 const News = () => {
-    return (
-        <div>
-            <Header></Header>
-            <div className='grid  md:grid grid-cols-4 mt-4'>
-                <div className='col-span-3'>
-                    <h2>News Details</h2>
-                    <Link to='/'>
-                        <button className='btn btn-secondary btn-sm'><FaArrowLeft /> All News in this Category</button>
-                    </Link>
-                </div>
-                <div>
-                    <RightSideNav />
-                </div>
+  const [newsDetails, setNewsDetails] = useState([]);
+  const { id } = useParams();
+
+  useEffect(() => {
+    fetch("/news.json")
+      .then((res) => res.json())
+      .then((data) => {
+        const filteredNews = data.find((news) => news._id === id);
+        setNewsDetails(filteredNews);
+      });
+  }, []);
+
+  console.log(newsDetails);
+
+  return (
+    <div>
+      <Header></Header>
+      <div className="grid  md:grid grid-cols-4 mt-4">
+        <div className="col-span-3">
+          <h2 className="text-xl mb-4 ">News Details</h2>
+          <div className="card card-compact bg-base-100 border  shadow-xl">
+            <figure>
+              <img
+              className="w-[97%] mx-auto mt-2"
+                src={newsDetails.image_url}
+                alt="Shoes"
+              />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title">{newsDetails.title}</h2>
+              <p>{newsDetails.details}</p>
+              <div className="card-actions justify-start mt-4">
+                <Link to='/'>
+                <button className="btn btn-sm btn-secondary"><FaArrowLeft /> All News in this Category</button>
+                </Link>
+              </div>
             </div>
+          </div>
         </div>
-    );
+        <div className="ml-2">
+          <RightSideNav />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default News;
